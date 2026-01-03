2ef072f7dd5629ff8c8c6605426411a4c857baf9
owoppenheimer <owoppenheimer@gmail.com>
2026-01-03 21:27:05 +0500

    main.rs: added cli args parser (yaaaay)

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,14 @@
-use std::{process::exit, env::args};
+use argh::FromArgs;
 
-const VERSION: &'static str = "0.1";
+#[derive(Debug, FromArgs)]
+/// A simple program that converts a number into specific number system.
+struct Arguments {
+    /// specify a number system.
+    #[argh(option, short = 'b')]
+    number_system: Option<u64>,
 
-fn print_help() {
-    println!("basenum ver {VERSION}.\n");
-    println!("A simple program that converts a number into a specific number system.");
-    println!("By default, this program uses a binary number system.");
-    println!("To specify a number system, you can use a -ns flag.");
-
-}
-
-// check all arguments and return index of number
-fn arg_check(args: &Vec<String>) -> (usize, Option<u64>) {
-    let mut index: usize = 1;
-
-    let mut number_system: Option<u64> = None;
-    for arg in args {
-        match arg.as_str() {
-            "--help" | "-h" => {
-                print_help();
-                exit(0);
-            }
-
-            "-ns" | "--number-system" => {
-                number_system = {
-                    Some(args[2].trim().parse().expect(&format!("{}: Couldn't parse a number. (u64)", args[2])))
-                };
-                index = 3;
-            }
-
-            _ => {}
-        }
-    }
-
-    return (index, number_system);
+    #[argh(positional)]
+    number: u64
 }
 
 // convert a number
@@ -46,30 +21,21 @@ fn to_base(mut num: u64, number_system: u64) -> String {
     let digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     while num > 0 {
-        let remainder = (num % number_system as u64) as usize;
+        let remainder = (num % number_system) as usize;
         result.push(digits.chars().nth(remainder).unwrap());
-        num /= number_system as u64;
+        num /= number_system;
     }
     result.chars().rev().collect()
 }
 
 fn main() {
-    let arg_list = args().collect::<Vec<String>>();
-    if arg_list.len() == 1 { print_help() ; exit(1) }
+    let number_system: Arguments = argh::from_env();
 
-    let num_index = arg_check(&arg_list).0;
-    let number_system = arg_check(&arg_list).1;
-
-    let num: u64 = arg_list[num_index].trim().parse().expect(&format!("{}: Couldn't parse a number (i64)", arg_list[1]));
-    match number_system {
-        Some(2) => println!("{num:b}"),
+    match number_system.number_system {
+        Some(2) | std::option::Option::None => println!("{:b}", number_system.number),
 
         Some(value) => {
-            println!("{}", to_base(num, value));
+            println!("{}", to_base(number_system.number, value));
         },
-
-        std::option::Option::None => {
-            println!("{num:b}");
-        }
     }
 }
